98859bcfd897ee98b7c76680e1ac55dfd3e6ce5b
andrea.fan <84858081+AndreaFan123@users.noreply.github.com>
2024-01-22 08:48:33 +0800

    Start working on footer

--- Challenge_10_sunnyside_agency/src/App.tsx
+++ Challenge_10_sunnyside_agency/src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import ImageListing from "./components/ImageListing";
 import ShowCase from "./components/ShowCase";
@@ -10,6 +11,7 @@ function App() {
       <ShowCase />
       <TestimonialListing />
       <ImageListing />
+      <Footer />
     </>
   );
 }
