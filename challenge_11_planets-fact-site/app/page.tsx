9ab3ccaf07dbbd66626b3aab811b901da59bffd1
andrea.fan <84858081+AndreaFan123@users.noreply.github.com>
2024-01-27 20:37:04 +0800

    Setting up project and import assets

--- challenge_11_planets-fact-site/app/page.tsx
+++ challenge_11_planets-fact-site/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main>
-      <h1>Hello</h1>
+    <main className="flex flex-col lg:gap-2 justify-center items-center w-full h-screen">
+      <h1 className="text-[5rem] lg:text-[10rem] text-white">Hello</h1>
+      <h2 className="text-white text-[3rem] lg:text-[5rem]">
+        Welcome to <span>planet Fact</span>
+      </h2>
     </main>
   );
 }
