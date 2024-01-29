354b97cdcca57c97f5e45f3eae0856aa68323d73
andrea.fan <84858081+AndreaFan123@users.noreply.github.com>
2024-01-29 23:25:32 +0800

    Complete navbar

--- challenge_11_planets-fact-site/app/[lang]/components/LangSwitcher.tsx
+++ challenge_11_planets-fact-site/app/[lang]/components/LangSwitcher.tsx
@@ -2,11 +2,13 @@
 
 // TODO: Make it like selector
 
-import { usePathname } from 'next/navigation';
+import { useState } from 'react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { i18n, type Locale } from '../../../i18n-config';
 
 export default function LangSwitcher() {
+  const [toggle, setToggle] = useState(false);
   const pathName = usePathname();
   const redirectedPathName = (locale: Locale) => {
     if (!pathName) return '/';
@@ -16,10 +18,10 @@ export default function LangSwitcher() {
   };
 
   return (
-    <ul className="text-[1.1rem] flex gap-3">
+    <ul className="w-1/2 flex p-4 text-[1rem] md:flex-row gap-3">
       {i18n.locales.map((locale) => {
         return (
-          <li key={locale}>
+          <li key={locale} className="border py-1 text-center px-2 rounded-md">
             <Link href={redirectedPathName(locale)}>{locale}</Link>
           </li>
         );
