c8e993f6471b437289b5d72006be4286e2371810
andrea.fan <84858081+AndreaFan123@users.noreply.github.com>
2024-02-07 23:54:41 +0800

    Update typography and animations

--- challenge_11_planets-fact-site/app/[lang]/page.tsx
+++ challenge_11_planets-fact-site/app/[lang]/page.tsx
@@ -6,10 +6,16 @@ export default async function Home({ params }: { params: { lang: Locale } }) {
 
   return (
     <main className="flex flex-col lg:gap-2 justify-center items-center w-full h-screen">
-      <h1 className="text-[5rem] lg:text-[10rem] text-white">
+      <h1
+        className="text-[5rem] lg:text-[10rem] text-white relative w-[max-content]
+before:absolute before:inset-0 before:animate-typewriter
+before:bg-white
+after:absolute after:inset-0 after:w-[0.125em] after:animate-caret
+after:bg-black"
+      >
         {dictionary.greeting['hello']}
       </h1>
-      <h2 className="text-white text-center text-[3rem] lg:text-[5rem]">
+      <h2 className=" text-white text-center text-[3rem] lg:text-[5rem]">
         {dictionary.greeting['welcome']}
       </h2>
     </main>
--- challenge_11_planets-fact-site/tailwind.config.ts
+++ challenge_11_planets-fact-site/tailwind.config.ts
@@ -33,6 +33,35 @@ const config: Config = {
         antonio: ['Antonio', 'sans-serif'],
         spartan: ['Spartan', 'sans-serif'],
       },
+      animation: {
+        typewriter: 'typewriter 2s steps(11) forwards',
+        caret:
+          'typewriter 2s steps(11) forwards, blink 1s steps(11) infinite 2s',
+      },
+      keyframes: {
+        typewriter: {
+          to: {
+            left: '100%',
+          },
+        },
+      },
+      blink: {
+        '0%': {
+          opacity: '0',
+        },
+        '0.1%': {
+          opacity: '1',
+        },
+        '50%': {
+          opacity: '1',
+        },
+        '50.1%': {
+          opacity: '0',
+        },
+        '100%': {
+          opacity: '0',
+        },
+      },
     },
   },
   plugins: [],
