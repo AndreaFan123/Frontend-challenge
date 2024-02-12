43637569dec4c63736031277a7a5a026aadd71fc
andrea.fan <84858081+AndreaFan123@users.noreply.github.com>
2024-02-12 12:02:47 +0800

    Remove unused import in LangSwitcher component

--- challenge_11_planets-fact-site/app/[lang]/components/LangSwitcher.tsx
+++ challenge_11_planets-fact-site/app/[lang]/components/LangSwitcher.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useState } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { i18n, type Locale } from '../../../i18n-config';
