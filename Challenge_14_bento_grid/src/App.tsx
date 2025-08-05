c07fc4e33f81423460b74810b0ad20704f1ad15e
Andrea Fan <shan32157@gmail.com>
2025-08-05 19:04:12 +0800

    Refactor: Remove unused components and styles
    - Deleted Text, Button, and Card components along with their associated CSS modules.
    - Simplified App component to render an empty main element.
    - Removed token generation scripts and related JSON files for colors, spacing, and typography.
    - Updated global CSS to include new font and color variables.
    - Added star.png image to the public directory.

--- Challenge_14_bento_grid/src/App.tsx
+++ Challenge_14_bento_grid/src/App.tsx
@@ -1,40 +1,7 @@
-import { Button } from "./components/button/Button";
-import { Text } from "./components/Text/Text";
-import { Card } from "./components/card/Card";
 import "./index.css";
 
 function App() {
-  return (
-    <main>
-      <div>
-        <Text as="h1" variant="primary">
-          Primary Text
-        </Text>
-        <Text as="h2" variant="secondary">
-          Secondary Text
-        </Text>
-        <Text as="h3" variant="light">
-          Light Text
-        </Text>
-        <Text as="h4" variant="ghost">
-          Ghost Text
-        </Text>
-        <Text as="p">Default Text</Text>
-      </div>
-      <div>
-        <Card variant="primary">Primary Card</Card>
-        <Card variant="secondary">Secondary Card</Card>
-        <Card variant="light">Light Card</Card>
-        <Card variant="sun">Sun Card</Card>
-        <Card variant="light-sun">Light Sun Card</Card>
-      </div>
-      <Button variant="primary">Primary</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="ghost" disabled>
-        Ghost Disabled
-      </Button>
-    </main>
-  );
+  return <main></main>;
 }
 
 export default App;
